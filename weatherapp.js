c25f610d14c095f783d191916e2c8e16ff688271
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2021-12-12 01:02:32 -0500

    Info Containers arranged properly

--- weatherapp.js
+++ weatherapp.js
@@ -31,12 +31,10 @@ subbutton.addEventListener(
         const humidity = document.getElementById("humidity");
         const clouds = document.getElementById("clouds");
 
-        console.log("hello");
-
         // output the inputs obtained from fetch() function the to dom
-        location.innerHTML += `<div class="output-text">  ${newData.name} in ${newData.sys.country} </div>`;
+        location.innerHTML += `<div class="output-text">  ${newData.name} in ${newData.sys.country} </div> <div>${newData.timezone}</div> `;
 
-        coordinates.innerHTML += `<ul class="output-list"> <li >Long: ${newData.coord.lon} <li> Lat: ${newData.coord.lat} </ul>`;
+        coordinates.innerHTML += `<ul class="output-list"> <li> ${newData.coord.lon}<sup>Lon</sup></li> <li> ${newData.coord.lat} <sup>Lat</sup></li> </ul>`;
 
         temp.innerHTML += `<div class="output-text">${newData.main.temp}<sup>&#8457</sup></div>`;
 
@@ -44,7 +42,7 @@ subbutton.addEventListener(
 
         humidity.innerHTML += `<div class="output-text"> ${newData.main.humidity}% </div> `;
 
-        clouds.innerHTML += `<div class="output-text">${newData.clouds.all}% </div> `;
+        clouds.innerHTML += `<div class="output-text">${newData.clouds.all} % </div> `;
 
         //reset location input
         locationinput.value = " ";
@@ -52,14 +50,65 @@ subbutton.addEventListener(
 
       .catch((err) => alert("Wrong City"));
 
+    // function getDate() {
+    //   let mydate = new Date();
+    //   console.log(mydate);
+
+    //   let my_utc_Offset = mydate.getTimezoneOffset();
+    //   console.log(my_utc_Offset);
+
+    //   mydate.setMinutes(mydate.getMinutes() + my_utc_Offset);
+
+    //   let locationOffset = -300;
+    //   mydate.setMinutes(mydate.getMinutes() + locationOffset);
+
+    //   console.log(mydate);
+    // }
+
+    // getDate();
+
     // rearranges Containers for Data
+
     let iconContainers = document.getElementsByClassName("icon-container");
-    function changeContainer() {
-      for (let i = 0; i < iconContainers.length; i++) {
-        iconContainers[i].setAttribute("style", "grid-column-end: 2");
+    let icons = document.querySelectorAll("i");
+    let infoCards = document.getElementsByClassName("info-card");
+    let outputText = document.getElementsByClassName("output-text");
+    // coordinates.style.display = "flex";
+    // coordinates.setAttribute("style", "grid-template-columns: 25% 75%;");
+
+    function postionText() {
+      for (let i = 0; outputText.length; i++) {
+        outputText[i].setAttribute(
+          "style",
+          "grid-column-start: 1; grid-column-end: 3"
+        );
       }
     }
 
+    function changeCards() {
+      for (let i = 0; i < infoCards.length; i++) {
+        infoCards[i].setAttribute(
+          "style",
+          "grid-template-rows: auto auto 50% ; "
+        );
+      }
+    }
+
+    function resizeIcons() {
+      for (let i = 0; i < icons.length; i++) {
+        icons[i].setAttribute("style", "font-size: 3rem");
+      }
+    }
+
+    function changeContainer() {
+      for (let i = 0; i < iconContainers.length; i++) {
+        iconContainers[i].setAttribute("style", "grid-column-end: 3");
+      }
+    }
+
+    postionText();
+    changeCards();
+    resizeIcons();
     changeContainer();
   }
 );
