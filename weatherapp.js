23d6724ab4c175fd82ce31e5e64583f499274b01
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2021-12-15 01:22:09 -0500

    More Responsive. Animations added.

--- weatherapp.js
+++ weatherapp.js
@@ -1,5 +1,5 @@
 let locationInput = document.getElementById("locationinput");
-let subButton = document.getElementById("subbutton");
+let subButton = document.getElementById("subButton");
 let iconContainers = document.getElementsByClassName("icon-container");
 let icons = document.querySelectorAll("i");
 let infoCards = document.getElementsByClassName("info-card");
@@ -9,13 +9,8 @@ subButton.addEventListener("click", runApp);
 
 function runApp() {
   // validateCycle();
-  // locationInput.value = "";
-  getInfo();
-  postionText();
-  resizeIcons();
 
-  // changeContainer();
-  // changeCards();
+  getInfo();
 }
 
 function validateCycle() {
@@ -54,9 +49,16 @@ function getInfo() {
       const wind = document.getElementById("wind");
       const humidity = document.getElementById("humidity");
       const clouds = document.getElementById("clouds");
+      let conditions = document.getElementById("conditions");
+      let dateSection = document.getElementById("date-section");
+
+      let currentDate = new Date();
+      console.log(currentDate);
 
       // output the inputs obtained from fetch() function the to dom
-      location.innerHTML += `<div class="output-text">  ${newData.name} in ${newData.sys.country} </div> <div>${newData.timezone}</div> `;
+
+      date.innerHTML += `${currentDate}`;
+      location.innerHTML += `<div class="output-text"> ${newData.name} in ${newData.sys.country} </div> <div>${newData.timezone}</div> `;
 
       coordinates.innerHTML += `<ul class="output-list"> <li> ${newData.coord.lon}<sup>Lon</sup></li> <li> ${newData.coord.lat} <sup>Lat</sup></li> </ul>`;
 
@@ -66,70 +68,30 @@ function getInfo() {
 
       humidity.innerHTML += `<div class="output-text"> ${newData.main.humidity}% </div> `;
 
-      clouds.innerHTML += `<div class="output-text">${newData.clouds.all} % </div> `;
+      clouds.innerHTML += `<div class="output-text">${newData.clouds.all}%</div> `;
+
+      conditions.innerHTML += `<div class="output-text">${newData.weather[0].description}</div>`;
 
       //reset location input
       locationInput.value.reload;
-})
+    })
     .catch((err) => alert("Wrong City"));
 }
 
 /// Functions below reposition data in containers
 
-function postionText() {
-  for (let i = 0; outputText.length; i++) {
-    outputText[i].setAttribute(
-      "style",
-      "grid-column-start: 1; grid-column-end: 3"
-    );
-  }
+function getTime(offset) {
+  let today = new Date();
+  let utc = today.getTime() + today.getTimezoneOffset() * 60000;
+  let nd = new Date(utc + 360000 * offset);
+  let a = new Date();
+  a.getUTCHours(7200);
+
+  return "the local Time of " + a.toLocaleString;
 }
 
-
-function resizeIcons() {
-  for (let i = 0; i < icons.length; i++) {
-    icons[i].setAttribute("style", "font-size: 3rem");
-  }
-}
-
-// ////////////////////////////////////
-
-// function changeCards() {
-//   for (let i = 0; i < infoCards.length; i++) {
-//     infoCards[i].setAttribute("style", "grid-template-rows: auto auto 50% ; ");
-//   }
 // }
 
-
-
-
-
-// function changeContainer() {
-//   for (let i = 0; i < iconContainers.length; i++) {
-//     iconContainers[i].setAttribute("style", "grid-column-end: 3");
-//   }
-// }
-
-// POTENTIAL FUNCTION TO ADD NEW DATA FEATURE
-// function fetchNews(countryCode, searchCat) {
-//   let apiKey = "8c379ea0a50b4c9aacc8c96068b7f82a";
-//   let bypass = `https://cors-anywhere.herokuapp.com/`;
-//   let bypass2 = `{headers: new Headers({"X-Requested-With":"macaroni"})  }`;
-//   let newsURL = `${bypass}https://newsapi.org/v2/top-headlines?country=${countryCode}&category=${searchCat}&apiKey=8c379ea0a50b4c9aacc8c96068b7f82a,${bypass2}`;
-
-//   fetch(newsURL)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const newsData = data;
-//       console.log(newsData);
-//     });
-// }
-
-
-
-// //////////////////////////////////////
 //TO DO LIST
-//Fix Refresh / Reset
-// Make Responsive
-//Add Feedback Behaviors
+//Fix Refresh / Reset / - General Control
 //Add time at location
