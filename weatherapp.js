e75dcc01fbc7ce3ce45def2ac23f5490fb8da869
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2022-01-01 22:46:54 -0500

    Icons fixed. Old data clears on new request.

--- weatherapp.js
+++ weatherapp.js
@@ -8,21 +8,22 @@ let outputText = document.getElementsByClassName("output-text");
 subButton.addEventListener("click", runApp);
 
 function runApp() {
-  // validateCycle();
-
   getInfo();
 }
 
-function validateCycle() {
-  if (locationInput !== "" && outputText.length > 2) {
-    location.reload();
-    getInfo();
-  } else {
-    alert("City Name Not Found");
-  }
+//Clears existing output text
+function clearData() {
+  let outputLists = document.querySelectorAll(".output-list");
+  let outputText = document.querySelectorAll(".output-text");
+  outputLists.forEach((item) =>
+    item !== null ? item.parentNode.removeChild(item) : item
+  );
+  outputText.forEach((item) =>
+    item !== null ? item.parentNode.removeChild(item) : item
+  );
 }
 
-// returns a new date object with the date and time set to the offset.
+// Returns a new date object with the date and time set to the offset.
 const getDate = (offset) => {
   let now = new Date();
   now.setTime(now.getTime() + now.getTimezoneOffset() * 60000); // utc
@@ -42,45 +43,36 @@ function getInfo() {
   fetch(thatURL)
     .then((response) => response.json())
     .then((data) => {
-      //verify input
-      console.log(`The location Input was ${city}`);
-      console.log(`Data was gathered for: ${city}`);
-      console.log(data);
       const newData = data;
+      // console.log(`Data was gathered for: ${city}`);
 
       // connect js to dom
-
       const location = document.getElementById("location");
-      const cityname = document.getElementById("");
       const coordinates = document.getElementById("coordinates");
       const temp = document.getElementById("temp");
       const wind = document.getElementById("wind");
       const humidity = document.getElementById("humidity");
       const clouds = document.getElementById("clouds");
       let conditions = document.getElementById("conditions");
-      let dateSection = document.getElementById("date-section");
 
-      // let currentDate = new Date();
-      // console.log(currentDate);
+      clearData();
 
-      // output the inputs obtained from fetch() function the to dom
+      location.innerHTML += `<div class="output-text"> ${newData.name} in ${newData.sys.country} </div> <div class="output-text timezone-text">${newData.timezone}</div> `;
 
-      // date.innerHTML += `${currentDate}`;
-      location.innerHTML = `<div class="output-text"> ${newData.name} in ${newData.sys.country} </div> <div>${newData.timezone}</div> `;
+      coordinates.innerHTML += `<ul class="output-list"> <li> ${newData.coord.lon}<sup>Lon</sup></li> <li> ${newData.coord.lat} <sup>Lat</sup></li> </ul>`;
 
-      coordinates.innerHTML = `<ul class="output-list"> <li> ${newData.coord.lon}<sup>Lon</sup></li> <li> ${newData.coord.lat} <sup>Lat</sup></li> </ul>`;
+      temp.innerHTML += `<div class="output-text">${newData.main.temp}<sup>&#8457</sup></div>`;
 
-      temp.innerHTML = `<div class="output-text">${newData.main.temp}<sup>&#8457</sup></div>`;
+      wind.innerHTML += `<ul class="output-list"> <li>Speed: ${newData.wind.speed}<sup>mph</sup></li> <li> Gust: ${newData.wind.gust} </ul> `;
 
-      wind.innerHTML = `<ul class="output-list"> <li>Speed: ${newData.wind.speed}<sup>mph</sup></li> <li> Gust: ${newData.wind.gust} </ul> `;
+      humidity.innerHTML += `<div class="output-text"> ${newData.main.humidity}% </div> `;
 
-      humidity.innerHTML = `<div class="output-text"> ${newData.main.humidity}% </div> `;
+      clouds.innerHTML += `<div class="output-text">${newData.clouds.all}%</div> `;
 
-      clouds.innerHTML = `<div class="output-text">${newData.clouds.all}%</div> `;
-
-      conditions.innerHTML = `<div class="output-text">${newData.weather[0].description}</div>`;
+      conditions.innerHTML += `<div class="output-text">${newData.weather[0].description}</div>`;
 
       getDate(data.timezone);
+
       //reset location input
       locationInput.value = "";
     })
